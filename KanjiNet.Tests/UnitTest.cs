e0ecdb1083c1a975d3f25a893ba07582e26d4216
Koichi-Kobayashi <Koichi-Kobayashi@users.noreply.github.com>
2025-09-15 11:25:35 +0900

    テストケース追加

--- KanjiNet.Tests/UnitTest.cs
+++ KanjiNet.Tests/UnitTest.cs
@@ -44,9 +44,25 @@ public class PublicApiTests
     }
 
     [Fact]
-    public void Isタカハシ()
+    public void To常用漢字()
     {
-        Assert.Equal("高", Kanji.ReplaceOldToNew("髙"));
-        Assert.Equal("橋", Kanji.ReplaceOldToNew("橋"));
+        Assert.Equal("吉田", Kanji.ReplaceOldToNew("𠮷田"));
+        Assert.Equal("富田", Kanji.ReplaceOldToNew("冨田"));
+        Assert.Equal("峰", Kanji.ReplaceOldToNew("峯"));
+        Assert.Equal("島崎", Kanji.ReplaceOldToNew("嶋﨑"));
+        Assert.Equal("徳", Kanji.ReplaceOldToNew("德"));
+        Assert.Equal("浜", Kanji.ReplaceOldToNew("濱"));
+        Assert.Equal("浜", Kanji.ReplaceOldToNew("濵"));
+        Assert.Equal("渡辺", Kanji.ReplaceOldToNew("渡邉"));
+        Assert.Equal("渡辺", Kanji.ReplaceOldToNew("渡邊"));
+        Assert.Equal("広", Kanji.ReplaceOldToNew("廣"));
+        Assert.Equal("斉藤", Kanji.ReplaceOldToNew("齊藤"));
+        Assert.Equal("斎藤", Kanji.ReplaceOldToNew("齋藤"));
+        Assert.Equal("斎藤", Kanji.ReplaceOldToNew("斎藤"));
+        Assert.Equal("桜井", Kanji.ReplaceOldToNew("櫻井"));
+        Assert.Equal("沢", Kanji.ReplaceOldToNew("澤"));
+        Assert.Equal("関", Kanji.ReplaceOldToNew("關"));
+        Assert.Equal("高橋", Kanji.ReplaceOldToNew("髙橋"));
+        Assert.Equal("柳", Kanji.ReplaceOldToNew("栁"));
     }
 }
